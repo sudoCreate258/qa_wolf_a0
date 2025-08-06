21362294fa590d9d93684dc488ee1adcd14efe50
sc258 <sudoCreate258@gmail.com>
2025-08-06 11:45:55 -0400

    added install + pseudocode

---
+++ install_scripts.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install nodejs npm
+node -v
+npm -v
+npm ci
+npm install -D playwright
+npx playwright install chromium
+npx playwright install firefox
+npx playwright install webkit
