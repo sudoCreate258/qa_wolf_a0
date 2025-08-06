363e4676aa6c0bfb7ec7c1b14d5b4cbc3e163305
sc258 <sudoCreate258@gmail.com>
2025-08-06 12:02:46 -0400

    pseudocode

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ async function sortHackerNewsArticles() {
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com/newest");
-
-  // TODO - validate + print first 100 articles (new to old)}
+    // TODO - validate + print first 100 articles (new to old)
+    // go to Hacker News
+    //input
+    await page.goto("https://news.ycombinator.com/newest");
+    //process - extract all 100 into an array
+    //        - parse time since post
+    //        - validate by time new to old (sort)
+    //        - prepare post for screen console
+    //output to screen
+}
 
 (async () => {
   await sortHackerNewsArticles();
