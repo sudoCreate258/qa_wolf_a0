29ccb840b27600e2b787fb3e13ed08d5fb8dc746
sc258 <sudoCreate258@gmail.com>
2025-08-07 01:12:25 -0400

    set variables in driver

--- index.js
+++ index.js
@@ -2,19 +2,28 @@
 const { chromium } = require("playwright");
 
 async function sortHackerNewsArticles() {
-  // launch browser
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+    // launch browser
+    const browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
 
-    // TODO - validate + print first 100 articles (new to old)
     // go to Hacker News
-    //input
     await page.goto("https://news.ycombinator.com/newest");
-    //process - extract all 100 into an array
-    //        - parse time since post
-    //        - validate by time new to old (sort)
-    //        - prepare post for screen console
+    
+    // TODO - validate + print first 100 articles (new to old)
+    //input - driver code
+    //variable declaration
+    let entry_lst = [];
+    let pageCount = 0;
+    let morePageFlag = true;
+
+    while (morePageFlag){
+        await loadRows(page); //allow pages to load
+        //process - extractts the reduces  all 100 into an array
+        //        - parse time since post
+        //        - validate by time new to old (sort)
+        //        - prepare post for screen console
+    }
     //output to screen
 }
 
