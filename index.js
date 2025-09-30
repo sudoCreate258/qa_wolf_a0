f408efd8e80a8b0903dbe30abbc3ca490e4dd464
sc258 <sudoCreate258@gmail.com>
2025-09-30 15:53:57 -0400

    updated loop logic

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
-// TODO - validate + print first 100 articles (new to old)
-/**
-class HN_Page{
-    constructor(page){}
 
-    async gotToSite( URL ){}
+/**class HN_Page{ //TODO validate + print first 100 articles (new to old)
+    constructor(page){}
+    gotToSite(URL){
     loadRoads(DEBUG) {}
     runPipeline(DEBUG){}
-    async extractEntries(){}
+    extractEntries(){}
     printToScreen()
-}
-**/
+}**/
 
 async function sortHackerNewsArticles() {
     const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
-    // -- create news page object
-    // go to Hacker News
-    //await page.goto("https://news.ycombinator.com/newest");
-    // -- goToSite( URL )
+    // -- create news page object -- go to Hacker News
+    // -- goToSite(URL="https://news.ycombinator.com/newest")
+
     let pageFlag = true; 
 
     while (pageFlag){
-        // -- loadRows(DEBUG) 
         // -- runPipeline(DEBUG)
         // -- updatePageFlag()
     }
