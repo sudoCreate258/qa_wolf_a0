34da8ee1326db3254a260113d94970054cecaa14
sc258 <sudoCreate258@gmail.com>
2025-10-02 01:08:12 -0400

    initial stage boiler plate code for class functions i.e., HN_Page Object

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
 
-/**class HN_Page{ //TODO validate + print first 100 articles (new to old)
-    constructor(page){}
+class HN_Page{ //TODO validate + print first 100 articles (new to old)
+    constructor(p){
+        this.page = p;
+        this.pageFlag = false;
+        //more button
+        //entry locator
+        //entry container
+    }
+    getPageFlag(){ return this.pageFlag;}
 
-    async gotToSite(URL){}
-    async loadRows(DEBUG){}
+    async gotToSite(URL){
+        await this.page.goto(URL);
+        return page;
+    }
+
+/*  async loadRows(DEBUG){}
     async extractEntries(){}
-
     runPipeline(DEBUG){}
-    updatePageFlag(){}
     printToScreen(){}
-}**/
+}*/
 
-async function sortHackerNewsArticles() {
+    async function sortHackerNewsArticles() {
     const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
     // -- create news page object -- go to Hacker News
-    // -- goToSite(URL="https://news.ycombinator.com/newest")
+    // -- page = goToSite(url="https://news.ycombinator.com/newest")
 
     let pageFlag = true; 
 
     while (pageFlag){
         // -- runPipeline(DEBUG)
-        // -- updatePageFlag()
+        // -- pageFlag = getPageFlag()
     }
 }
 
