be6859a303e196e21dab16e9398742d23698f17c
sc258 <sudoCreate258@gmail.com>
2025-10-14 17:37:29 -0400

    basic acces to html elem: title, link; seeking time/age

--- index.js
+++ index.js
@@ -1,45 +1,66 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
 
-class HN_Page{ //TODO validate + print first 100 articles (new to old)
+//TODO validate + print first 100 articles (new to old)
+class HN_Page{ 
     constructor(p,u){
+        this.entries = [];
         this.page = p;
-        this.url = u;
-        this.pageFlag = true;
+        this.url  = u;
+        this.rlocate = null;
     }
     
-    async goToSite(){
+    async parseRow(r){
+        const raw_title = r.locator('span.titleline > a');
+        const sub_title = await raw_title.innerText();
+
+        const link = await raw_title.getAttribute('href');
+
+        console.log(`${sub_title} ${link}`);
+        //const link = r.locator('href');
+        //const age = r.locator('span.age');
+
+        return {
+            sub_title: sub_title.trim() ,
+            link: link,
+        };
+    }
+
+    async visitPage(){
         await this.page.goto(this.url);
+        this.rlocate = this.page.locator('tr.athing');
     }
 
-    runPipeline(DEBUG=true){
-        this.pageFlag = false;
-        this.goToSite();
-/*      async loadRows(DEBUG){}
-        async extractEntries(){}
-            //more button
-            //entry locator
-            //entry container
-        printToScreen(){}*/
+    async extractEntries(){
+        const rows = await this.rlocate.all();
+        for(const r of rows){
+            const parsed = await this.parseRow(r);
+            this.entries.push(parsed);
+        }
     }
 
-    getPageFlag(){
-        return this.pageFlag;
+    printToScreen(){
+        console.log("Extracted Entries:");
+        for(const e of this.entries){
+            console.log(`${e}`);
+        }
     }
+
+    async runPipeline(){//loop such that 4 max iterations on more button
+        await this.visitPage();
+        await this.extractEntries();
+        //this.printToScreen();
+    }   
 }
 
-    async function sortHackerNewsArticles() {
+async function sortHackerNewsArticles() {
     const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
-    // -- create news page object -- go to Hacker News
-    let hpage = new HN_Page(page,"https://news.ycombinator.com/newest")
-    let pageFlag = hpage.getPageFlag();
 
-    while(pageFlag){
-        hpage.runPipeline()
-        pageFlag = hpage.getPageFlag();
-    }
+    let hpg = new HN_Page(page,"https://news.ycombinator.com/newest");
+    await hpg.runPipeline();    // -- run object function
+//await browser.close();      // -- close playwright features
 }
 
 (async () => {
